15958d8642e55ca056aac604db6c4c4e3dca4b75
Royhan2D <royhan256@gmail.com>
2017-05-20 22:58:15 +0700

    Update EKTP.java

--- src/tugasss/EKTP.java
+++ src/tugasss/EKTP.java
@@ -31,3 +31,21 @@ public class TugasEKTP {
         RT4.add(new penduduk("Nulman yeasim", "kristen", "perempuan", "Wiraswasta", "AB", "kartika", "03", "makasar", "18-06-1993", "Indonesia"));
 
         
+        //DESA2
+        ArrayList<penduduk> RT5 = new ArrayList<>();
+        RT5.add(new penduduk("Tirta Anugrah", "Islam", "Laki-laki", "Tentara", "O", "Subur makmur", "05", "jogjakarta", "11-02-1995", "indonesia"));
+        RT5.add(new penduduk("Stella Cristiani", "Kristen", "Perempuan", "pedagang", "O","Subur makmu", "05", "Papua", "06-05-1993", "indonesia"));       
+        
+        
+        ArrayList<penduduk> RT6 = new ArrayList<>();
+        RT6.add(new penduduk("Nina Antari", "Islam", "PerempuDan", "Mahasiswa", "B", "Subur Makmur", "06", "sumatra", "01-12-1997", "Indonesia"));
+        RT6.add(new penduduk("Valin Furin", "Islam", "Perempuan", "Guru", "A", "Subur makmur", "06", "banyuwangi", "09-06-1995", "Indonesia"));
+
+        ArrayList<penduduk> RT7 = new ArrayList<>();
+        RT7.add(new penduduk("Turin Karti", "kristen", "perempuan", "Wiraswasta", "AB", "subur makmur", "07", "malang", "19-05-1993", "Indonesia"));
+        RT7.add(new penduduk("Titania Asifa", "Islam", "perempuan", "pedagang", "AB", "subur makmur", "07", "pandaan", "07-06-1996", "Indonesia"));
+
+        ArrayList<penduduk> RT8 = new ArrayList<>();
+        RT8.add(new penduduk("Denis Rananda", "hindu", "laki-laki", "polisi", "O", "Subur makmur", "08", "makasar", "18-10-1998", "Indonesia"));
+        RT8.add(new penduduk("Toni Tikno", "Islam", "laki-laki", "polisi", "A", "Subur makmur", "08", "tuban", "10-06-1993", "Indonesia"));
+
