2390be3954865b5f62ead60d08b352875dbd27f5
vallerian2D <vallenkloningan@gmail.com>
2017-05-20 22:51:47 +0700

    Update EKTP.java

--- src/tugasss/EKTP.java
+++ src/tugasss/EKTP.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugasss;
 
-/**
- *
- * @author USER
- */
-public class EKTP {
-    
-}
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TugasEKTP {
+
+    public static void main(String[] args) {
+        
+        //penduduk pdk = new penduduk();
+
+        
+        ArrayList<penduduk> RT1 = new ArrayList<>();
+        RT1.add(new penduduk("Debora Rizky", "Islam", "Perempuan", "Mahasiswa", "A", "Kartika", "01", "Madiun", "10-03-1998", "indonesia"));
+        RT1.add(new penduduk("Royhan Ramadhan", "Islam", "Laki-laki", "wiraswasta", "O", "Kartika", "01", "Surabaya", "11-010-1998", "indonesia"));       
+        
+        
+        ArrayList<penduduk> RT2 = new ArrayList<>();
+        RT2.add(new penduduk("Vallerian Ariel", "Hindu", "Perempuan", "Mahasiswa", "B", "Kartika", "02", "Bali", "23-03-1998", "Indonesia"));
+        RT2.add(new penduduk("Albertos Janah", "Hindu", "Perempuan", "Mahasiswa", "B", "Kartika", "02", "Bali", "23-03-1998", "Indonesia"));
+
+        ArrayList<penduduk> RT3 = new ArrayList<>();
+        RT3.add(new penduduk("Jumaidah Subhi", "kristen", "perempuan", "Wiraswasta", "AB", "kartika", "03", "makasar", "18-06-1993", "Indonesia"));
+        RT3.add(new penduduk("Yaani Hita", "kristen", "perempuan", "Wiraswasta", "AB", "kartika", "03", "makasar", "18-06-1993", "Indonesia"));
+
+        ArrayList<penduduk> RT4 = new ArrayList<>();
+        RT4.add(new penduduk("Hajar Dewanta", "kristen", "perempuan", "Wiraswasta", "AB", "kartika", "03", "makasar", "18-06-1993", "Indonesia"));
+        RT4.add(new penduduk("Nulman yeasim", "kristen", "perempuan", "Wiraswasta", "AB", "kartika", "03", "makasar", "18-06-1993", "Indonesia"));
+
+        
